762e161248f5f241a92e623e73279f318cb7dff9
iamlahirurandika <lahirurandika@hotmail.com>
2022-06-02 18:06:38 +0530

    Created Register and Login View Models and Added Required Data Fields

---
+++ UserIdentity/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserIdentity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View(); 
+        }
+    }
+}
