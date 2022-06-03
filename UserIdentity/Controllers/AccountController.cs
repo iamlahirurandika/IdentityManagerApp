56e07a84bb90c8dd3a91988b88cc4035d44868bb
iamlahirurandika <lahirurandika@hotmail.com>
2022-06-03 11:55:09 +0530

    Implemented returnUrl to Privacy-> Login and Just Login

--- UserIdentity/Controllers/AccountController.cs
+++ UserIdentity/Controllers/AccountController.cs
@@ -24,20 +24,22 @@ namespace UserIdentity.Controllers
 
         //Register GET
         [HttpGet]
-        public async Task<IActionResult> Register()
+        public async Task<IActionResult> Register(string? returnurl = null)
         {
             //Return RegisterViewModel
+            ViewData["ReturnUrl"] = returnurl;
             RegisterViewModel registerViewModel = new RegisterViewModel(); 
             return View(registerViewModel);  
         }
 
         //Register POST
         [HttpPost]
-        public async Task<IActionResult> Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model, string? returnurl = null)
         {
+            ViewData["ReturnUrl"] = returnurl; 
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState);
+                return View(model);
             }
             //Populating new user object here
             var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
@@ -48,6 +50,10 @@ namespace UserIdentity.Controllers
                 //This means the user is created 
                 //In order to sign in the user we need to use SignInManager || We are gonna use it using Dependency Injection 
                 await _signInManager.SignInAsync(user, isPersistent: false);
+                if (returnurl != null)
+                {
+                    return LocalRedirect(returnurl);
+                }
                 return RedirectToAction("Index", "Home");
             }
             //Creating a helper action method below to add errors 
@@ -89,7 +95,7 @@ namespace UserIdentity.Controllers
                 {
                     if (returnurl != null)
                     {
-                        return Redirect(returnurl);
+                        return LocalRedirect(returnurl);
                     }
                     return RedirectToAction("Index", "Home");
                 }
